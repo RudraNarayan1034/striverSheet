b6fee0e91b98740042dfb0aef8caecb0f32608fd
RUDRA NARAYAN MISHRA <rudranarayanmishra1034@gmail.com>
2024-02-28 22:34:28 +0530

    Selection Sort

---
+++ ar_selectionSort.java
@@ -0,0 +1,19 @@
+public class ar_selectionSort {
+    public static void selectionSort(int[] arr) {
+        //Your code goes here
+        int n=arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int mini = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[mini]) {
+                    mini = j;
+                }
+            }
+            //swap
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    
+}
