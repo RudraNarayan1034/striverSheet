a85423086f62e26fd8a57af4a3499ebf5bbcda81
RUDRA NARAYAN MISHRA <rudranarayanmishra1034@gmail.com>
2024-03-05 22:29:29 +0530

    binary Search in array

---
+++ ar_binarySearch.java
@@ -0,0 +1,16 @@
+public class ar_binarySearch {
+    public static int search(int []nums, int target) {
+        // Write your code here.
+        int n = nums.length; //size of the array.
+        int low = 0, high = n - 1;
+
+        // Perform the steps:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) return mid;
+            else if (target > nums[mid]) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
+    }
+}
