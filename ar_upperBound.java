33b1106f3d6e2cad27d0a84602d2f22e2e879c59
RUDRA NARAYAN MISHRA <rudranarayanmishra1034@gmail.com>
2024-03-07 22:10:37 +0530

    search upper bound in array

---
+++ ar_upperBound.java
@@ -0,0 +1,20 @@
+public class ar_upperBound {
+    public static int upperBound(int []arr, int x, int n){
+        // Write your code here.
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] > x) {
+                ans = mid;
+                //look for smaller index on the left
+                high = mid - 1;
+            } else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+}
