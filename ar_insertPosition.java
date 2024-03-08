7ac7e310aa1ed0bfca1da0eea8e90c95605a3bbb
RUDRA NARAYAN MISHRA <rudranarayanmishra1034@gmail.com>
2024-03-08 20:43:42 +0530

    search insert position  in array

---
+++ ar_insertPosition.java
@@ -0,0 +1,21 @@
+public class ar_insertPosition {
+    public static int searchInsert(int [] arr, int m){
+        // Write your code here.
+        int n = arr.length; // size of the array
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] >= m) {
+                ans = mid;
+                //look for smaller index on the left
+                high = mid - 1;
+            } else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+}
