154e20db3bdc33697f4f7c229090e6b096cd59eb
RUDRA NARAYAN MISHRA <rudranarayanmishra1034@gmail.com>
2024-04-03 23:09:31 +0530

    move zeroes in the array

---
+++ ar_moveZeroes.java
@@ -0,0 +1,30 @@
+public class ar_moveZeroes{
+    public static int[] moveZeros(int n, int []a) {
+        // Write your code here.
+        int j = -1;
+        //place the pointer j:
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        //no non-zero elements:
+        if (j == -1) return a;
+
+        //Move the pointers i and j
+        //and swap accordingly:
+        for (int i = j + 1; i < n; i++) {
+            if (a[i] != 0) {
+                //swap a[i] & a[j]:
+                int tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+                j++;
+            }
+        }
+        return a;
+    
+    }
+}
