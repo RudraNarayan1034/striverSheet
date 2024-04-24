cff13b67aaf31500b166c5c030f885ca2ba28726
RUDRA NARAYAN MISHRA <rudranarayanmishra1034@gmail.com>
2024-04-24 21:01:49 +0530

    a

---
+++ a.java
@@ -0,0 +1,23 @@
+import java.math.BigInteger;
+import java.util.LinkedList;
+import java.util.*;
+public class a {
+    public static void main(String[] args){
+        Scanner scanner =new Scanner(System.in);
+        /*int count =0;
+        while(scanner.hasNextLine()){
+            scanner.nextLine();
+            count++;
+        }*/
+       // System.out.println("Enter the number");
+       // int singleByte = System.in.read();
+        //byte b[] = System.in.readALLbyte();
+        System.out.println("Enter the numbe:");
+       // Linkedlist<Integer> l = new LinkedList<>();
+        //int y2=20;
+        //l.add(x);//AutoBoxing
+        //l.add(new Integer(x));//manual Boxing
+        //BigInteger b = new BigInteger(val:"8936453784928131243546574231");
+       // BigInteger b2 = new BigInteger(val:"123456789098765432112345678987654");
+    }
+}
