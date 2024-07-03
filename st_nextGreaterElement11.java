15a600dc62ca01a55c7fa11be1568d5f41d9499d
RUDRA NARAYAN MISHRA <rudranarayanmishra1034@gmail.com>
2024-07-03 18:10:06 +0530

    Next greater element 2 in the stack

---
+++ st_nextGreaterElement11.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int result[]=new int[nums.length];
+        Stack<Integer>stack=new Stack<>();
+        //two pass
+        //store index(duplicate elements)
+        for(int i=2*nums.length-1;i>=0;i--){
+            while(!stack.isEmpty()&&nums[i%nums.length]>=nums[stack.peek()]){
+                stack.pop();
+            }
+            result[i %nums.length]=stack.isEmpty()?-1:nums[stack.peek()];
+            stack.push(i%nums.length);
+        }
+        return result;
+    }
+}
